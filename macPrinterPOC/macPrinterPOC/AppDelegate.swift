39326e22899c5cb038d6204a26206186ca84140b
Satendra Touch <satendradagar@gmail.com>
2020-04-18 13:46:57 +0530

    Added base project setup

---
+++ macPrinterPOC/macPrinterPOC/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  macPrinterPOC
+//
+//  Created by Satendra Singh on 18/04/20.
+//  Copyright © 2020 CoreBits Software Solutions Pvt Ltd. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ macPrinterPOC/macPrinterPOC/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  macPrinterPOC
+//
+//  Created by Satendra Singh on 18/04/20.
+//  Copyright © 2020 CoreBits Software Solutions Pvt Ltd. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
