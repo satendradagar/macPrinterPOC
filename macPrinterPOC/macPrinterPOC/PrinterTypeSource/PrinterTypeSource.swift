617d4f6809ee43f2281c194b3a53f3d98772adda
Satendra <>
2020-05-12 20:14:26 +0530

    added type sources

---
+++ macPrinterPOC/macPrinterPOC/PrinterTypeSource/PrinterTypeSource.swift
@@ -0,0 +1,15 @@
+//
+//  PrinterTypeSource.swift
+//  macPrinterPOC
+//
+//  Created by Satendra Dagar on 11/05/20.
+//  Copyright © 2020 CoreBits Software Solutions Pvt Ltd. All rights reserved.
+//
+
+import Foundation
+class PrinterTypeSource {
+    
+    class func sources() -> [String]{
+        return ["Teslin", " Inkjet PVC"]
+    }
+}
